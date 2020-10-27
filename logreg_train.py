4241aca6123eeb95d207de9d1c62c4362b46117f
lchappon <lchappon@student.42.fr>
2020-10-27 15:01:08 +0100

    logreg train

--- logreg_train.py
+++ logreg_train.py
@@ -3,9 +3,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-learning_rate = 0.07
-n_iterations = 1000
+learning_rate = 0.01
+n_iterations = 2000
 corr_range = 0.7
+
+def corr_fillna(df):
+    for v in df.columns:
+        for w in df.columns:
+            if df[v].corr(df[w]) >= corr_range and v != w:
+                df[v] = df[v].fillna(df[w])
+                df[w] = df[w].fillna(df[v])
+            elif df[v].corr(df[w]) <= -corr_range and v != w:
+                df[v] = df[v].fillna(-df[w])
+                df[w] = df[w].fillna(-df[v])
+    return df
 
 def model(X, theta):
     return 1 / (1 + np.exp(-X.dot(theta)))
@@ -16,14 +27,14 @@ def cost_function(X, theta, target):
 def gradient(X, theta, target):
     return X.T.dot(model(X, theta) - target) / len(target)
 
-def gradient_descent(X, target, learning_rate, n_iterations):
+def gradient_descent(X, target):
     theta = np.random.randn(X.shape[1], 1)
     cost_history = np.zeros(n_iterations)
 
     for i in range(n_iterations):
-        theta = theta - learning_rate * gradient(X, theta)
+        theta = theta - learning_rate * gradient(X, theta, target)
         cost_history[i] = cost_function(X, theta, target)
-    
+
     return theta, cost_history
 
 def coef_determination(target, predictions):
@@ -32,16 +43,10 @@ def coef_determination(target, predictions):
 
 	return 1 - u / v
 
-def corr_fillna(df):
-    for v in df.columns:
-        for w in df.columns:
-            if df[v].corr(df[w]) > corr_range and v != w:
-                df[v] = df[v].fillna(df[w])
-                df[w] = df[w].fillna(df[v])
-            elif df[v].corr(df[w]) < -corr_range and v != w:
-                df[v] = df[v].fillna(-df[w])
-                df[w] = df[w].fillna(-df[v])
-    return df
+def plt_cost_history(cost_history, house, c):
+    plt.xlabel('n_iterations')
+    plt.ylabel('cost_history')
+    plt.plot(range(n_iterations), cost_history, c=c, label=house)
 
 def main():
     if len(sys.argv) != 2:
@@ -54,13 +59,41 @@ def main():
     df = corr_fillna(df)
     df.insert(0, 'Hogwarts House', hs)
     df = df.dropna(axis=0)
-    target = df['Hogwarts House']
+
     features = df.drop('Hogwarts House', axis=1).to_numpy()
+    X = np.hstack((features, np.ones((features.shape[0], 1))))
+
+    target = df['Hogwarts House']
     target_g = target.replace({'Gryffindor':1, 'Slytherin':0, 'Ravenclaw':0, 'Hufflepuff':0}).to_numpy().reshape(-1, 1)
     target_s = target.replace({'Gryffindor':0, 'Slytherin':1, 'Ravenclaw':0, 'Hufflepuff':0}).to_numpy().reshape(-1, 1)
     target_r = target.replace({'Gryffindor':0, 'Slytherin':0, 'Ravenclaw':1, 'Hufflepuff':0}).to_numpy().reshape(-1, 1)
     target_h = target.replace({'Gryffindor':0, 'Slytherin':0, 'Ravenclaw':0, 'Hufflepuff':1}).to_numpy().reshape(-1, 1)
-    X = np.hstack((features, np.ones((features.shape[0], 1))))
+
+    theta_g, cost_history_g = gradient_descent(X, target_g)
+    theta_s, cost_history_s = gradient_descent(X, target_s)
+    theta_r, cost_history_r = gradient_descent(X, target_r)
+    theta_h, cost_history_h = gradient_descent(X, target_h)
+
+    thetas = np.column_stack((theta_g, theta_s, theta_r, theta_h))
+    np.savetxt('thetas.csv', thetas, delimiter=',', header='Gryffindor, Slytherin, Ravenclaw, Hufflepuff', comments='')
+
+    predictions_g = model(X, theta_g)
+    predictions_s = model(X, theta_s)
+    predictions_r = model(X, theta_r)
+    predictions_h = model(X, theta_h)
+    print("Coefficient of determination for Gryffindor: {}".format(coef_determination(target_g, predictions_g)))
+    print("Coefficient of determination for Slytherin: {}".format(coef_determination(target_s, predictions_s)))
+    print("Coefficient of determination for Ravenclaw: {}".format(coef_determination(target_r, predictions_r)))
+    print("Coefficient of determination for Hufflepuff: {}".format(coef_determination(target_h, predictions_h)))
+
+    plt.figure()
+    plt.title('Cost history')
+    plt_cost_history(cost_history_g, "Gryffindor", 'r')
+    plt_cost_history(cost_history_s, "Slytherin", 'g')
+    plt_cost_history(cost_history_r, "Ravenclaw", 'b')
+    plt_cost_history(cost_history_h, "Hufflepuff", 'y')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
